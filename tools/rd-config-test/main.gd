91108a0fa4a7a725103341f96b2301be1cb58132
monkeyx-net <tim@monkeyx.net>
2024-07-11 14:21:19 +0100

    On branch feat/godot-uk  Changes to be committed: 	modified:   rd-config-test/main.gd 	modified:   rd-config-test/main.tscn 	modified:   rd-config-test/project.godot
     Changes not staged for commit:
    	modified:   ../rd-submodules/ryujinx (modified content)

--- tools/rd-config-test/main.gd
+++ tools/rd-config-test/main.gd
@@ -4,10 +4,43 @@ extends Control
 	# Manually populating emulator values etc. These could be parsed from batch files?
 	# Get $rdhome $rd_logs_folder etc values etc? Detect if if in gaming mode or Desktop Mode
 	# Some Godot variables there already
+	# basic log function to show what actions have been taken and what scripts run.
+
+func _ready():
+	var file_path = "../../tools/configurator.sh"
+	var emulator_list = get_emulator_list_from_system_path(file_path)
+	print(emulator_list)
+
+func get_emulator_list_from_system_path(file_path: String):
+	var output = []
+	var command = "sed -n '/local emulator_list=(/,/)/{s/.*local emulator_list=\\(.*\\)/\\1/; /)/q; p}' " + file_path
+	var exit_code = OS.execute("sh", ["-c", command], output, )
+	if exit_code == 0:  
+		var content = array_to_string(output)
+		return parse_emulator_list(content)
+	else:
+		print("Error reading file: ", exit_code)
+		return {}
+
+func parse_emulator_list(content: String) -> Dictionary:
+	var emulator_dict = {}
+	var regex = RegEx.new()
+	regex.compile(r'"([^"]+)"\s*"([^"]+)"')
+	var matches = regex.search_all(content)
+	
+	for match in matches:
+		var name = match.get_string(1)
+		var description = match.get_string(2)
+		emulator_dict[name] = description
+
+	return emulator_dict
 
 func _on_command_button_pressed():
-	var command = "ls"
-	var parameters = ["-l", "/tmp"]
+	#var command = "ls"
+	#var parameters = ["-ltr", "/tmp"]
+	var command = "find"
+	var parameters = ["$HOME/", "-name", "es_systems.xml","-print"]
+	print (command ,parameters)
 	var result = execute_command(command, parameters, false)
 	$Main_TabContainer/SETTINGS/ScrollContainer/DisplayRichTextLabel.text = result["output"]
 	$Main_TabContainer/SETTINGS/CommandExitLabel.text = "Exit Code: " + result["exit_code"]
@@ -19,7 +52,7 @@ func execute_command(command: String, params: Array, blocking: bool) -> Dictiona
 	var output = []
 	var exit_code = OS.execute(command, params, output, blocking)
 	# Check for errors
-	output = array_to_text(output)
+	output = array_to_string(output)
 	
 	if exit_code != OK:
 		exit_code = str(exit_code)
@@ -50,15 +83,14 @@ func _on_option_button_font_item_selected(index):
 			custom_theme = preload("res://themes/dyslexia_theme.tres")
 			$Main_TabContainer.theme = custom_theme
 
-
-func array_to_text(arr: Array) -> String:
+func array_to_string(arr: Array) -> String:
 	var text = ""
-	for i in arr:
-		text += String(i)
+	for line in arr:
+		text += line + "\n" #String(line)
 	return text
 
-
-
 func _on_emulator_button_pressed():
+	$Main_TabContainer/SETTINGS/OptionButton/EmulatorButton/ConfirmationDialog.visible=true
 	var something = $Main_TabContainer/SETTINGS.get_tab_title($Main_TabContainer/SETTINGS.current_tab) + " - "
 	$Main_TabContainer/SETTINGS/ScrollContainer/DisplayRichTextLabel.text= something + $Main_TabContainer/SETTINGS/OptionButton.text + "!"
+
